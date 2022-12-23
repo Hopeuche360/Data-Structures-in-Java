25d759c017faff60c90731babe7a1e650f7a3e68
Hopeuche360 <hopechijuka@gmail.com>
2022-12-23 09:48:56 +0100

    recent changes

---
+++ src/main/java/org/example/WorkingWithLists.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkingWithLists {
+    public static void main(String[] args) {
+        List colors = new ArrayList();
+    }
+}
